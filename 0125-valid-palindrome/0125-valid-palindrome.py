57a1597502ee73e503aec4bc0c57a3faa9a63484
Gwendolyn Howes <163925688+gwen-howes@users.noreply.github.com>
2024-10-29 17:01:31 -0600

    Update 0125-valid-palindrome.py

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        sClean = ''.join(char for char in s if char.isalnum()).lower()
+        l, r = 0, len(s)-1
 
-        i, j = 0, len(sClean)-1
-
-        while(i < j):
-            if sClean[i] != sClean[j]:
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while r > l and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
                 return False
-            
-            i += 1
-            j -= 1
-
+            l, r = l + 1, r - 1
         return True
