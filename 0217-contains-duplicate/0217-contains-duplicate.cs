9ff9761bf10ced5fe1ecdf209bdeb5367f19be37
Gwendolyn Howes <163925688+gwen-howes@users.noreply.github.com>
2024-11-22 12:02:26 -0700

    Time: 9 ms (44.23%), Space: 55.3 MB (90.66%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -1,10 +1,5 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        HashSet<int> seen = new HashSet<int>();
-        for (int i=0; i<nums.Length; i++) {
-            seen.Add(nums[i]);
-        }
-        return seen.Count != nums.Length;
-        
+        return new HashSet<int>(nums).Count != nums.Length;
     }
 }
